6a3ec9a11740176f5444082b21c5e370cfa6143b
David Pink <dvdpink@gmail.com>
2019-10-31 21:18:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,8 +4,8 @@ function addElementToBeginningOfArray (array, element){
   return [element,...array];
 }
 function destructivelyAddElementToBeginningOfArray (array, element) {
-   array.unshift (element);
-   return array;
+   return array.unshift (element);
+   
 }
 
 function addElementToEndOfArray (array, element) {
