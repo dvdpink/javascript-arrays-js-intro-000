ab0dc1e7332cdd3afe31e2ca49fbcdc171d353d9
David Pink <dvdpink@gmail.com>
2019-10-31 20:11:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,7 @@ function accessElementInArray (array, index) {
 function destructivelyRemoveElementFromBeginningOfArray (array) {
   return array.shift()
 }
+
+function removeElementFromBeginningOfArray (array) {
+  return array.slice(1)
+}
