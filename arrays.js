c99dbf4bd359233392e92d071f1f11c23b77265b
David Pink <dvdpink@gmail.com>
2019-10-31 16:59:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,3 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray (myArray,newElement){
 var myArray = [1,2];
 return [. . .newElement,myArray};
-
